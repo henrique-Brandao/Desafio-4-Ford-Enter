29bdc297e2e4d7f37bb1c392a8efffbaee09ba5f
Henrique Gabriel dos Santos Brandão <67274638+henrique-Brandao@users.noreply.github.com>
2025-04-07 10:51:55 -0300

    Commit
    adicionando os Termos e Condições da empresa no formulário e modificando o botão

--- Assets/JS/form.js
+++ Assets/JS/form.js
@@ -1,17 +1,32 @@
 function mostrarAlerta(event) {
-    event.preventDefault();
-
-    Swal.fire({
-        title: "Sucesso!",
-        text: "Formulário enviado com sucesso!",
-        icon: "success",
-        confirmButtonText: "OK",
-        customClass: {
-            title: "titulo-popup",
-            popup: "popup",
-            confirmButton: "botao-popup"
-        }
-    }).then(() => {
-        document.getElementById("formulario").reset();
-    });
+    event.preventDefault()
+        Swal.fire({
+            title: "Sucesso!",
+            text: "Formulário enviado com sucesso!",
+            icon: "success",
+            confirmButtonText: "OK",
+            customClass: {
+                title: "titulo-popup",
+                popup: "popup",
+                confirmButton: "botao-popup"
+            }
+        }).then(() => {
+            document.getElementById("formulario").reset();
+        });
 }
+
+
+const input = document.getElementById('inputTermoCondicoes')
+const botao = document.getElementsByClassName('botaoDesativado')[0]
+
+input.addEventListener('change', () => {
+    if (!input.checked) {
+        botao.disabled = true
+        botao.classList.replace('botaoAtivado', 'botaoDesativado')
+    } else {
+        botao.classList.replace('botaoDesativado', 'botaoAtivado')
+
+        botao.disabled = false
+    }
+})
+
